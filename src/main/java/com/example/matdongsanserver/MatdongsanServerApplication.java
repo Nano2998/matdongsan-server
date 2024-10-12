e2e5c301a46606f5b6af764c2c31b306cc270b03
EunHakKim <acky529@gmail.com>
2024-10-12 22:46:33 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/example/matdongsanserver/MatdongsanServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.matdongsanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatdongsanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatdongsanServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/matdongsanserver/MatdongsanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matdongsanserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatdongsanServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
