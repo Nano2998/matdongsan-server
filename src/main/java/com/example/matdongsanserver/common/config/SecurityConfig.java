d9d13ffc6c1ac61136ac51ce20bcf18369c2642b
EunHakKim <acky529@gmail.com>
2024-10-15 16:32:34 +0900

    feat: SecurityConfig 일부 수정 및 cors 처리 코드 추가

--- src/main/java/com/example/matdongsanserver/common/config/SecurityConfig.java
+++ src/main/java/com/example/matdongsanserver/common/config/SecurityConfig.java
@@ -1,13 +1,24 @@
 package com.example.matdongsanserver.common.config;
 
+import com.example.matdongsanserver.domain.auth.filter.JwtFilter;
 import com.example.matdongsanserver.domain.auth.handler.OAuth2SuccessHandler;
 import com.example.matdongsanserver.domain.auth.service.KakaoMemberDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -16,23 +27,51 @@ public class SecurityConfig {
 
     private final KakaoMemberDetailsService kakaoMemberDetailsService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final JwtFilter customJwtFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
         http
-                .csrf((csrf) -> csrf.disable());
+                .cors((httpSecurityCorsConfigurer ->
+                        httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource())));
         http
-                .formLogin((login) -> login.disable());
+                .csrf(AbstractHttpConfigurer::disable);
         http
-                .httpBasic((basic) -> basic.disable());
+                .formLogin(AbstractHttpConfigurer::disable);
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+        http
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http
                 .oauth2Login(oAuth2Login -> {
                     oAuth2Login.userInfoEndpoint(userInfoEndpointConfig ->
                     userInfoEndpointConfig.userService(kakaoMemberDetailsService));
-                    oAuth2Login.successHandler(oAuth2SuccessHandler)
+                    oAuth2Login.successHandler(oAuth2SuccessHandler);
                 });
+        http
+                .addFilterBefore(customJwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        FilterRegistrationBean bean= new FilterRegistrationBean(new CorsFilter(source));
+
+        bean.setOrder(0);
+        return source;
     }
 }
