24dae59c8a6b8f7c12cf4af841bfa0f5504965fc
EunHakKim <acky529@gmail.com>
2024-12-03 16:34:33 +0900

    fix: 레디스 비밀번호 설정하도록 수정

--- src/main/java/com/example/matdongsanserver/common/config/RedisConfig.java
+++ src/main/java/com/example/matdongsanserver/common/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
@@ -17,8 +19,16 @@ public class RedisConfig {
     @Value("${spring.datasource.redis.port}")
     private int redisPort;
 
+    @Value("${spring.datasource.redis.password}")
+    private String redisPassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(redisHost);
+        configuration.setPort(redisPort);
+        configuration.setPassword(RedisPassword.of(redisPassword));
+
+        return new LettuceConnectionFactory(configuration);
     }
 }
