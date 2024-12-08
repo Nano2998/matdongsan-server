1dc49bebeb1173722276c093c3724db1ca077b99
Nano2998 <158575667+Nano2998@users.noreply.github.com>
2024-12-08 12:12:49 +0900

    style: 주석으로 변경

--- src/main/java/com/example/matdongsanserver/domain/module/controller/ModuleController.java
+++ src/main/java/com/example/matdongsanserver/domain/module/controller/ModuleController.java
@@ -1,26 +1,26 @@
-package com.example.matdongsanserver.domain.module.controller;
+// package com.example.matdongsanserver.domain.module.controller;
 
-import com.example.matdongsanserver.domain.module.service.ModuleService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+// import com.example.matdongsanserver.domain.module.service.ModuleService;
+// import io.swagger.v3.oas.annotations.Operation;
+// import io.swagger.v3.oas.annotations.tags.Tag;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Module API", description = "모듈 API")
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/modules")
-public class ModuleController {
+// @Tag(name = "Module API", description = "모듈 API")
+// @RestController
+// @RequiredArgsConstructor
+// @RequestMapping("/api/modules")
+// public class ModuleController {
 
-    private final ModuleService moduleService;
+//     private final ModuleService moduleService;
 
-    @Operation(summary = "모듈로 TTS 실행")
-    @GetMapping("/send/{storyId}")
-    public ResponseEntity<Void> sendCommand(
-            @PathVariable String storyId
-    ) {
-        moduleService.sendCommand(storyId);
-        return ResponseEntity.noContent().build();
-    }
-}
+//     @Operation(summary = "모듈로 TTS 실행")
+//     @GetMapping("/send/{storyId}")
+//     public ResponseEntity<Void> sendCommand(
+//             @PathVariable String storyId
+//     ) {
+//         moduleService.sendCommand(storyId);
+//         return ResponseEntity.noContent().build();
+//     }
+// }
