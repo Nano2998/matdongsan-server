4fa47814051a9af8b5b8310897e2baaa74dcccfe
EunHakKim <acky529@gmail.com>
2024-12-24 00:19:44 +0900

    #50 refactor: 자기 자신은 팔로우할 수 없게 수정

--- src/main/java/com/example/matdongsanserver/domain/follow/exception/FollowErrorCode.java
+++ src/main/java/com/example/matdongsanserver/domain/follow/exception/FollowErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum FollowErrorCode implements ErrorCode {
     FOLLOW_ALREADY_EXISTS(HttpStatus.NOT_FOUND, "이미 팔로우하였습니다."),
-    FOLLOW_NOT_EXISTS(HttpStatus.NOT_FOUND, "팔로우가 존재하지 않습니다.");
+    FOLLOW_NOT_EXISTS(HttpStatus.NOT_FOUND, "팔로우가 존재하지 않습니다."),
+    CANNOT_FOLLOW_MYSELF(HttpStatus.BAD_REQUEST, "본인은 팔로우할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
